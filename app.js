145d44a4d63edb0dfd9d3b82f79241ad98503023
Matt Cosentino <matt@learnedmedia.com>
2017-08-18 18:13:43 -0400

    starting to add cookies

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'pug');
 
 // handles GET requests for root route
 app.get('/', (req, res) => {
-	// res.send('<h1>Express test</h1>');
 	res.render('index');
 });
 
@@ -13,10 +15,14 @@ app.get('/cards', (req, res) => {
 	res.render('card', { prompt: "Who is buried in Grant's Tomb?" });
 });
 
-// app.get('/cards', (req, res) => {
-// 	res.locals.prompt = "Who is buried in Grant's Tomb?"
-// 	res.render('card');
-// });
+app.get('/hello', (req, res) => {
+	res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+	res.cookie('username', req.body.username);
+	res.render('hello', { name: req.body.username });
+});
 
 app.listen(3000, () => {
 	console.log('the application is running on localhost:3000!')
